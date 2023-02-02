d9cdc2e4b3a08f61e6ed1c3225cff4e21e3fdad5
Vinicius-gif <vfpv@hotmail.com>
2023-02-01 23:52:14 -0300

    ...

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,6 +1,7 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
 const AdministracaoRestaurantes = () => {
@@ -17,18 +18,36 @@ const AdministracaoRestaurantes = () => {
       })
   }, [])
 
+  const excluir = (restauranteExcluido: IRestaurante) => {
+    axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteExcluido.id}/`)
+    .then(resposta => {
+        const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteExcluido.id);
+        setRestaurantes([...listaRestaurante]);
+      })
+  }
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Nome</TableCell>
+            <TableCell>Editar</TableCell>
+            <TableCell>Excluir</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
             <TableCell>
               {restaurante.nome}
+            </TableCell>
+            <TableCell>
+              [<Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>]
+            </TableCell>
+            <TableCell>
+              <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>
+                Excluir
+              </Button>
             </TableCell>
           </TableRow>
           )}
